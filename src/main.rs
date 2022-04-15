bed6805d453ca60c4be3762f5c42320aee8700b6
Bert Guns <bertguns@gmail.com>
2022-04-15 12:59:13 +0200

    Add crossterm dependency. Create simple main function to enter alternative screen and wait for keypress.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    event::{self, Event, KeyCode, KeyEvent},
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand, Result,
+};
+use std::io::{stdout, Write};
+
+fn main() -> Result<()> {
+    let mut stdout = stdout();
+    stdout.execute(EnterAlternateScreen)?;
+    terminal::enable_raw_mode()?;
+
+    loop {
+        if let Ok(Event::Key(KeyEvent {
+            code: KeyCode::Char(_),
+            ..
+        })) = event::read()
+        {
+            break;
+        }
+    }
+
+    terminal::disable_raw_mode()?;
+    stdout.execute(LeaveAlternateScreen)?;
+    Ok(())
 }
