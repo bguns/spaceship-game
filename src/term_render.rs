81f71f9fe94dcbb44815049b3fef5edcccbea41e
Bert Guns <bertguns@gmail.com>
2022-05-25 20:54:01 +0200

    Add centered error message if screen is too small.

--- src/term_render.rs
+++ src/term_render.rs
@@ -27,38 +27,67 @@ impl Renderer {
     }
 
     pub fn render_frame(&mut self, game_state: &GameState) -> Result<()> {
-        self.stdout.queue(cursor::SavePosition)?;
         self.stdout.queue(cursor::MoveToColumn(1))?;
         self.stdout.queue(cursor::MoveToRow(1))?;
-        self.stdout.queue(Clear(ClearType::CurrentLine))?;
+        self.stdout.queue(Clear(ClearType::All))?;
         self.stdout.queue(style::Print(&format!(
             "Frame {} processed in {} microseconds{}",
             game_state.frame_number,
             game_state.now.elapsed().as_micros(),
             ".".repeat((game_state.frame_number % 60) as usize)
         )))?;
-        self.stdout.queue(cursor::MoveToRow(2))?;
-        self.stdout.queue(cursor::MoveToColumn(1))?;
-        self.stdout.queue(Clear(ClearType::CurrentLine))?;
+
         let (columns, rows) = size()?;
-        self.stdout.queue(style::Print(&format!(
-            "Terminal width x height: {} x {}. Desired dimensions: ",
-            columns, rows
-        )))?;
         if columns < 200 || rows < 50 {
-            self.stdout
-                .queue(style::PrintStyledContent("200 x 50".red()))?;
+            self.show_size_error(columns, rows)?;
         } else {
-            self.stdout
-                .queue(style::PrintStyledContent("200 x 50".green()))?;
+            self.stdout.queue(cursor::SavePosition)?;
+            self.stdout.queue(cursor::MoveToRow(2))?;
+            self.stdout.queue(cursor::MoveToColumn(1))?;
+            self.stdout.queue(Clear(ClearType::CurrentLine))?;
+            self.stdout.queue(style::Print(&format!(
+                "Terminal width x height: {} x {}.",
+                columns, rows
+            )))?;
+            self.stdout.queue(cursor::RestorePosition)?;
         }
-        self.stdout.queue(cursor::RestorePosition)?;
 
         for c in game_state.keyboard_state.get_pressed_characters() {
             self.stdout.queue(style::Print(c))?;
         }
 
         self.stdout.flush()?;
+        Ok(())
+    }
+
+    fn show_size_error(&mut self, current_columns: u16, current_rows: u16) -> Result<()> {
+        let middle_column = current_columns / 2;
+        let middle_row = current_rows / 2;
+        let error_message = "Please resize your terminal window until it is at least 200 columns wide and 50 rows high.";
+        let error_message_length = error_message.chars().count() as u16;
+        let half_error_message_length = error_message_length / 2;
+        let mut x = middle_column.saturating_sub(half_error_message_length);
+        let mut y = middle_row.saturating_sub(1);
+
+        let stdout = &mut self.stdout;
+        stdout.queue(cursor::SavePosition)?;
+        stdout.queue(cursor::MoveToColumn(x))?;
+        stdout.queue(cursor::MoveToRow(y))?;
+        stdout.queue(style::Print(error_message))?;
+
+        y += 1;
+        let current_size_message = &format!(
+            "Current colums x rows: {} x {}",
+            current_columns, current_rows
+        );
+        let half_current_size_message_length = current_size_message.chars().count() as u16 / 2;
+        x = middle_column.saturating_sub(half_current_size_message_length);
+        stdout.queue(cursor::MoveToColumn(x))?;
+        stdout.queue(cursor::MoveToRow(y))?;
+        stdout.queue(style::Print(current_size_message))?;
+
+        stdout.queue(cursor::RestorePosition)?;
+
         Ok(())
     }
 }
@@ -71,7 +100,6 @@ impl Drop for Renderer {
         while event::poll(Duration::from_millis(1)).unwrap_or(false) {
             let _ = event::read().expect("Unexpected crossterm error: event::read() returned Err after succesful event::poll.");
         }
-
         terminal::disable_raw_mode()
             .expect("Unexpected crossterm error: failed to disable raw mode on exit.");
         self.stdout
