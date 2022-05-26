1dd64fbe6a96c6acc1a0862197daf32255f6745b
Bert Guns <bertguns@gmail.com>
2022-05-26 15:11:02 +0200

    Add termintal logging for more timing information (total runtime, accumulated runtime, delta time).

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod input;
 use device_query::{DeviceState, Keycode};
 
 use winit::{
+    dpi::LogicalSize,
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
@@ -15,15 +16,20 @@ use crate::error::Result;
 use crate::input::KeyboardState;
 
 pub struct GameState {
-    frame_number: u64,
+    start_time: Instant,
     now: Instant,
+    delta_time: Duration,
+    run_time: Duration,
+    frame_number: u64,
     keyboard_state: KeyboardState,
     should_quit: bool,
 }
 
 impl GameState {
-    pub fn update(&mut self) -> Result<()> {
-        self.now = Instant::now();
+    pub fn update(&mut self, now: Instant) -> Result<()> {
+        self.delta_time = now - self.now;
+        self.run_time += self.delta_time;
+        self.now = now;
         self.frame_number += 1;
         self.keyboard_state.update(self.frame_number);
         self.should_quit = self
@@ -39,9 +45,13 @@ impl GameState {
 fn main() -> Result<()> {
     let device_state = DeviceState::new();
     let keyboard_state = KeyboardState::new(device_state);
+    let now = Instant::now();
     let mut game_state = GameState {
+        start_time: now,
+        now: now,
+        delta_time: Duration::from_millis(0),
+        run_time: Duration::from_millis(0),
         frame_number: 0,
-        now: Instant::now(),
         keyboard_state,
         should_quit: false,
     };
@@ -49,7 +59,12 @@ fn main() -> Result<()> {
     let sixteen_millis = Duration::from_millis(16);
 
     let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let window = WindowBuilder::new()
+        .with_title("Game")
+        .with_inner_size(LogicalSize::new(1920.0, 1080.0))
+        //.with_decorations(false)
+        .build(&event_loop)
+        .unwrap();
 
     event_loop.run(move |event, _, control_flow| match event {
         Event::WindowEvent {
@@ -60,25 +75,37 @@ fn main() -> Result<()> {
             *control_flow = ControlFlow::Exit
         }
         Event::MainEventsCleared => {
-            let previous_frame_start = game_state.now.clone();
-            game_state.update().unwrap();
-
-            let elapsed = game_state.now.elapsed();
-            let max_fps = 1_000_000.0 / elapsed.as_micros() as f64;
-            let fps = 1_000_000.0 / (game_state.now - previous_frame_start).as_micros() as f64;
-            print!(
-                "\rFrame number: {}; FPS: {:.2}; Max FPS: {:.2}",
-                game_state.frame_number, fps, max_fps
-            );
-            if elapsed < sixteen_millis {
-                *control_flow = ControlFlow::WaitUntil(game_state.now + sixteen_millis - elapsed);
+            let now = Instant::now();
+            let previous_frame_start = game_state.now;
+            if now - previous_frame_start < sixteen_millis {
+                *control_flow = ControlFlow::WaitUntil(previous_frame_start + sixteen_millis)
             } else {
-                *control_flow = ControlFlow::Poll;
-            }
+                game_state.update(now).unwrap();
+                // RENDER HERE
 
-            if game_state.should_quit {
-                println!("Should quit");
-                *control_flow = ControlFlow::Exit;
+                let elapsed = now.elapsed();
+                let max_fps = 1_000_000.0 / elapsed.as_micros() as f64;
+                let fps = 1_000_000.0 / game_state.delta_time.as_micros() as f64;
+                print!(
+                    "\rElapsed time: {}; Runtime: {}; dt: {:.2}, Frame number: {}; FPS: {:.2}; Max FPS: {:.2}",
+                    game_state.start_time.elapsed().as_millis(),
+                    game_state.run_time.as_millis(),
+                    game_state.delta_time.as_micros() as f64 / 1_000.0,
+                    game_state.frame_number,
+                    fps,
+                    max_fps
+                );
+
+                if elapsed < sixteen_millis {
+                    *control_flow = ControlFlow::WaitUntil(now + sixteen_millis - elapsed);
+                } else {
+                    *control_flow = ControlFlow::Poll;
+                }
+
+                if game_state.should_quit {
+                    println!("Should quit");
+                    *control_flow = ControlFlow::Exit;
+                }
             }
         }
         _ => {}
